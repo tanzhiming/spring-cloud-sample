6927e94aa19d7f56dfada5ae617197be99778b5b
tanzhiming <tanzhiminggood@163.com>
2019-02-19 17:39:04 +0800

    auth server

--- auth-server/src/main/java/com/tzm/springcloud/authserver/config/AuthorizationServerConfig.java
+++ auth-server/src/main/java/com/tzm/springcloud/authserver/config/AuthorizationServerConfig.java
@@ -1,6 +1,7 @@
 package com.tzm.springcloud.authserver.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -27,6 +28,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     private AuthenticationManager authenticationManager;
 
     @Autowired
+    @Qualifier("customUserDetailService")
     private UserDetailsService userDetailsService;
 
     @Autowired
