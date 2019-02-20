18e61287a08dcafbee0d0f906a68bf2331a7d196
tanzhiming <tanzhiminggood@163.com>
2019-02-20 17:37:39 +0800

    auth server

---
+++ gateway/src/main/java/com/tzm/springcloud/gateway/WebServerSecurityConfig.java
@@ -0,0 +1,26 @@
+package com.tzm.springcloud.gateway;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableOAuth2Sso
+@Order(0)
+@Slf4j
+public class WebServerSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        LOG.debug("HttpSecurity configure method");
+        http.antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/", "/login**")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+    }
+}
